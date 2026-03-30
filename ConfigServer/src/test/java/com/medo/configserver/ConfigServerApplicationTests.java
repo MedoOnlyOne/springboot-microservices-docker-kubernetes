de86436236e89c53ad91613ed06fd12c109efae5
= <=>
2026-03-30 16:44:50 +0200

    create config server

---
+++ ConfigServer/src/test/java/com/medo/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medo.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
